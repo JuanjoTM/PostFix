3ce6f88498d9910f6f8481dcb3b74e8df87a449b
Digo Regalado <Digo Regalado@172.17.12.93>
2014-07-21 23:02:58 -0600

    Segunda Version

--- PostFixweb/src/postfixweb/StackVector.java
+++ PostFixweb/src/postfixweb/StackVector.java
@@ -1,3 +1,11 @@
+/*Universidad del Valle de Guatemala
+  Algoritmos y Estructura de Datos
+  Autores:
+  Juan Jose Tzun 	Carnet: 13017
+  Carlos Cordero	Carnet: 13152
+  Luis Tello		Carnet: 13161
+  Fecha: 21/07/2014
+*/
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
